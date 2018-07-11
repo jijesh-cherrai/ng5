2383de3780e35642d55c70e7d095a72105e2ff31
jijesh <jijesh.c@hotmail.com>
2018-07-11 17:43:34 +0530

    l

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
